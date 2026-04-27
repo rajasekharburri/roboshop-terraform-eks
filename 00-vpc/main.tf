0d5d97fe0f7b85753999ee8c49833e2b70dd64a2
rajasekhar burri <burriraja@gmail.com>
2026-04-28 03:02:43 +0530

    jenkins

--- 00-vpc/main.tf
+++ 00-vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source = "git::https://github.com/daws-86s/terraform-aws-vpc.git?ref=main"
+    source = "git::https://github.com/rajasekharburri/terraform-aws-vpc.git"
     # VPC
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
